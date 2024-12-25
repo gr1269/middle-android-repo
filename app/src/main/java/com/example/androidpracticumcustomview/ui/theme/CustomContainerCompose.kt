05be7d3390a40cea863a739e994c4ee4ad3e3787
g.dyussupov <gr12@bk.ru>
2024-12-25 15:14:43 +0500

    Sprint 1

--- app/src/main/java/com/example/androidpracticumcustomview/ui/theme/CustomContainerCompose.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/ui/theme/CustomContainerCompose.kt
@@ -1,8 +1,23 @@
 package com.example.androidpracticumcustomview.ui.theme
 
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
 
 /*
 Задание:
@@ -17,18 +32,66 @@ fun CustomContainerCompose(
     firstChild: @Composable (() -> Unit)?,
     secondChild: @Composable (() -> Unit)?
 ) {
-    // Блок создания и инициализации переменных
-    // ..
 
-    // Блок активации анимации при первом запуске
+    val configuration = LocalConfiguration.current
+    val screenHeight = configuration.screenHeightDp.dp
+
+    var firstChildVisibility by remember { mutableStateOf(false) }
+    var secondChildVisibility by remember { mutableStateOf(false) }
+
+
     LaunchedEffect(Unit) {
-        // TODO
-        // ...
+        firstChildVisibility = true
+        delay(2000)
+        secondChildVisibility = true
     }
 
-    // Основной контейнер
-    Box() {
-        // TODO
-        // ...
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        val slideInYTop = animateDpAsState(
+            targetValue = if(firstChildVisibility) 0.dp else screenHeight/2, // Двигаемся к верхней части
+            animationSpec = tween(durationMillis = 5000)
+        )
+
+        val fadeInFirst = animateFloatAsState(
+            targetValue = if(firstChildVisibility) 1f else 0f,
+            animationSpec = tween(durationMillis = 1000)
+        )
+
+        Box(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .offset(y = slideInYTop.value)
+                .graphicsLayer(alpha = fadeInFirst.value)
+        ) {
+            if (firstChild != null) {
+                firstChild()
+            }
+        }
+
+        val slideInYBottom = animateDpAsState(
+            targetValue = if(secondChildVisibility) 0.dp else -screenHeight/2,
+            animationSpec = tween(durationMillis = 5000)
+        )
+
+        val fadeInSecond = animateFloatAsState(
+            targetValue = if(secondChildVisibility) 1f else 0f,
+            animationSpec = tween(durationMillis = 1000)
+        )
+
+
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .offset(y = slideInYBottom.value)
+                .graphicsLayer(alpha = fadeInSecond.value)
+        ) {
+            if (secondChild != null) {
+                secondChild()
+            }
+        }
+
     }
 }
