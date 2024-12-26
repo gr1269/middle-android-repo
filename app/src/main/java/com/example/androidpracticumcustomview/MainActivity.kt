78d06ee11a042e479b1b196bcc7cbfd6effba671
German <59974285+gr1269@users.noreply.github.com>
2024-12-26 16:54:36 +0500

    Revert "Sprint 1"

--- app/src/main/java/com/example/androidpracticumcustomview/MainActivity.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/MainActivity.kt
@@ -1,16 +1,11 @@
 package com.example.androidpracticumcustomview
 
-import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.Gravity.CENTER_HORIZONTAL
-import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import com.example.androidpracticumcustomview.ui.theme.CustomContainer
-import com.example.androidpracticumcustomview.ui.theme.MainScreen
 
 /*
 Задание:
@@ -23,9 +18,9 @@ class MainActivity : ComponentActivity() {
         /*
         Раскомментируйте нужный вариант
          */
-//        startXmlPracticum() // «традиционный» android (XML)
-        setContent { // Jetpack Compose
-            MainScreen() }
+        startXmlPracticum() // «традиционный» android (XML)
+//          setContent { // Jetpack Compose
+//             MainScreen()
     }
 
     private fun startXmlPracticum() {
@@ -33,34 +28,14 @@ class MainActivity : ComponentActivity() {
         setContentView(customContainer)
 
         val firstView = TextView(this).apply {
-            text = "First Child"
-            // Стиль, размеры и прочее для первого элемента
-            setTextColor(Color.BLACK)
-            setPadding(16, 16, 16, 16)
-            textSize = 18f
-            layoutParams = FrameLayout.LayoutParams(
-                FrameLayout.LayoutParams.WRAP_CONTENT,
-                FrameLayout.LayoutParams.WRAP_CONTENT
-            ).apply {
-                gravity = CENTER_HORIZONTAL
-            }
+            // TODO
+            // ...
         }
 
         val secondView = TextView(this).apply {
-            text = "Second Child"
-            // Стиль, размеры и прочее для второго элемента
-            setTextColor(Color.BLACK)
-            setPadding(16, 16, 16, 16)
-            textSize = 18f
-            layoutParams = FrameLayout.LayoutParams(
-                FrameLayout.LayoutParams.WRAP_CONTENT,
-                FrameLayout.LayoutParams.WRAP_CONTENT
-            ).apply {
-                gravity = CENTER_HORIZONTAL
-            }
+            // TODO
+            // ...
         }
-
-        customContainer.addView(firstView)
 
         // Добавление второго элемента через некоторое время
         Handler(Looper.getMainLooper()).postDelayed({
--- app/src/main/java/com/example/androidpracticumcustomview/ui/theme/MainScreen.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/ui/theme/MainScreen.kt
@@ -3,9 +3,7 @@ package com.example.androidpracticumcustomview.ui.theme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,14 +24,12 @@ fun MainScreen() {
 
             CustomContainerCompose(
                 firstChild = {
-                    Button(onClick = { }) {
-                        Text("First Child")
-                    }
+                    // TODO
+                    // ...
                 },
                 secondChild = {
-                    Button(onClick = { }) {
-                        Text("Second Child")
-                    }
+                    // TODO
+                    // ...
                 }
             )
         }
